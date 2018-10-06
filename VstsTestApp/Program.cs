f3f6c4e6796eeab985b00485ab6e30658874c0e4
Usama Azhar <Usama.Azhar@bentley.com>
2018-10-06 16:01:52 +0500

    First commit

---
+++ VstsTestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VstsTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test App for integration of vsts with git");
+        }
+    }
+}
